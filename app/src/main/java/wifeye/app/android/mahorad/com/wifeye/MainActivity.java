033a176f9734d40179f82d611a3513f84a2a9b71
Mahan Rad <mahorad@yahoo.com>
2017-05-11 00:14:21 +0200

    floating action button toggling for service behaviour

--- app/src/main/java/wifeye/app/android/mahorad/com/wifeye/MainActivity.java
+++ app/src/main/java/wifeye/app/android/mahorad/com/wifeye/MainActivity.java
@@ -2,21 +2,20 @@ package wifeye.app.android.mahorad.com.wifeye;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.ToggleButton;
 
 import javax.inject.Inject;
 
@@ -33,26 +32,17 @@ public class MainActivity extends ActivityManagePermission
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private ColorStateList greens;
+    private ColorStateList accent;
+
     private final Presenter presenter = new Presenter(this);
 
     private final FragmentSummary summary = new FragmentSummary();
+
     private DrawerLayout drawer;
     private NavigationView navigationView;
     private Toolbar toolbar;
-    private FloatingActionButton floatingButton;
-
-    private TextView serviceText;
-    private TextView wifiText;
-    private TextView stateText;
-    private TextView stateDate;
-    private TextView ssidText;
-    private TextView ssidDate;
-    private TextView ctidText;
-    private TextView ctidDate;
-    private TextView actionText;
-    private TextView actionDate;
-    private EditText persistence;
-    private ToggleButton service;
+    private FloatingActionButton fab;
 
     @Inject
     SharedPreferences preferences;
@@ -61,7 +51,6 @@ public class MainActivity extends ActivityManagePermission
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupUserInterface();
         presenter.onCreate();
         MainApplication.mainComponent().inject(this);
 
@@ -96,91 +85,6 @@ public class MainActivity extends ActivityManagePermission
         presenter.onDestroy();
     }
 
-    private void setupUserInterface() {
-//        serviceText = (TextView) findViewById(R.id.service);
-//        stateText = (TextView) findViewById(R.id.state);
-//        stateDate = (TextView) findViewById(R.id.stateDate);
-//        ssidText = (TextView) findViewById(R.id.ssid);
-//        ssidDate = (TextView) findViewById(R.id.ssidDate);
-//        ctidText = (TextView) findViewById(R.id.ctid);
-//        ctidDate = (TextView) findViewById(R.id.ctidDate);
-//        actionText = (TextView) findViewById(R.id.action);
-//        actionDate = (TextView) findViewById(R.id.actionDate);
-//        wifiText = (TextView) findViewById(R.id.wifi);
-//        persistence = (EditText) findViewById(R.id.persistence);
-//        service = (ToggleButton) findViewById(R.id.serviceButton);
-    }
-
-    public void toggleService(View view) {
-        ToggleButton b = (ToggleButton) view;
-        if (b.isChecked()) {
-            presenter.handlePermissions();
-        } else
-            presenter.stopMainService();
-    }
-
-
-    @Override
-    public void updateServiceState(final boolean enabled, final String date) {
-        runOnUiThread(() -> serviceText.setText(enabled ? "ENABLE" : "DISABLE"));
-    }
-
-    @Override
-    public void updateActionState(final Action action, final String date) {
-        runOnUiThread(() -> {
-            actionText.setText(action.toString());
-            actionDate.setText(date);
-        });
-    }
-
-    @Override
-    public void updatePersistence(String repository) {
-        runOnUiThread(() -> persistence.setText(repository));
-    }
-
-    @Override
-    public void updateWifiDeviceState(WifiState state) {
-        runOnUiThread(() -> wifiText.setText(state.toString()));
-    }
-
-    @Override
-    public void toggleMainServiceDisable() {
-        runOnUiThread(() -> service.setChecked(false));
-    }
-
-    @Override
-    public void updateTowerIdState(final String ctid, final String date) {
-        runOnUiThread(() -> {
-            ctidText.setText(ctid);
-            ctidDate.setText(date);
-        });
-    }
-
-    @Override
-    public void updateHotspotState(final String ssid, final String date) {
-        runOnUiThread(() -> {
-            ssidText.setText(ssid == null ? "" : ssid);
-            ssidDate.setText(date);
-        });
-    }
-
-    @Override
-    public void updateEngineState(final String state, String date) {
-        runOnUiThread(() -> {
-            stateText.setText(state);
-            stateDate.setText(date);
-        });
-    }
-
-
-
-
-
-
-
-
-
-
     private void setupToolbarView() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -201,10 +105,14 @@ public class MainActivity extends ActivityManagePermission
     }
 
     private void setupFloatingButton() {
-        floatingButton = (FloatingActionButton) findViewById(R.id.fab);
-        floatingButton.setOnClickListener(view -> Snackbar
-                .make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setSelected(false);
+        greens = new ColorStateList(
+                new int[][] { new int[0] },
+                new int[]   { ResourcesCompat.getColor(getResources(), R.color.colorGreen, null) });
+        accent = new ColorStateList(
+                new int[][] { new int[0] },
+                new int[]   { ResourcesCompat.getColor(getResources(), R.color.colorAccent, null) });
     }
 
     @Override
@@ -244,4 +152,85 @@ public class MainActivity extends ActivityManagePermission
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
+
+    public void toggleService(View view) {
+        fab.setSelected(!fab.isSelected());
+        if (fab.isSelected()) {
+            startService(view);
+        } else {
+            stopService(view);
+        }
+    }
+
+    private void stopService(View view) {
+        Snackbar
+                .make(view, "Service Stopped", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+        fab.setImageResource(R.drawable.eye_off);
+        fab.setBackgroundTintList(accent);
+//            presenter.stopMainService();
+    }
+
+    private void startService(View view) {
+        Snackbar
+                .make(view, "Service Started", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+        fab.setImageResource(R.drawable.eye);
+        fab.setBackgroundTintList(greens);
+//            presenter.handlePermissions();
+    }
+
+
+    @Override
+    public void updateServiceState(final boolean enabled, final String date) {
+//        runOnUiThread(() -> serviceText.setText(enabled ? "ENABLE" : "DISABLE"));
+    }
+
+    @Override
+    public void updateActionState(final Action action, final String date) {
+//        runOnUiThread(() -> {
+//            actionText.setText(action.toString());
+//            actionDate.setText(date);
+//        });
+    }
+
+    @Override
+    public void updatePersistence(String repository) {
+//        runOnUiThread(() -> persistence.setText(repository));
+    }
+
+    @Override
+    public void updateWifiDeviceState(WifiState state) {
+//        runOnUiThread(() -> wifiText.setText(state.toString()));
+    }
+
+    @Override
+    public void toggleMainServiceDisable() {
+//        runOnUiThread(() -> service.setChecked(false));
+    }
+
+    @Override
+    public void updateTowerIdState(final String ctid, final String date) {
+//        runOnUiThread(() -> {
+//            ctidText.setText(ctid);
+//            ctidDate.setText(date);
+//        });
+    }
+
+    @Override
+    public void updateHotspotState(final String ssid, final String date) {
+//        runOnUiThread(() -> {
+//            ssidText.setText(ssid == null ? "" : ssid);
+//            ssidDate.setText(date);
+//        });
+    }
+
+    @Override
+    public void updateEngineState(final String state, String date) {
+//        runOnUiThread(() -> {
+//            stateText.setText(state);
+//            stateDate.setText(date);
+//        });
+    }
+
 }
