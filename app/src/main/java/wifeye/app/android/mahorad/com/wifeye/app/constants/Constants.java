cabbccba834c91d5d6a42e834368101c8f71f748
Mahan Rad <mahorad@yahoo.com>
2017-06-05 14:06:59 +0200

    cleanup

--- app/src/main/java/wifeye/app/android/mahorad/com/wifeye/app/constants/Constants.java
+++ app/src/main/java/wifeye/app/android/mahorad/com/wifeye/app/constants/Constants.java
@@ -5,8 +5,8 @@ import permission.auron.com.marshmallowpermissionhelper.PermissionUtils;
 public class Constants {
 
     public static final int OBSERVE_REPEAT_COUNT = 10;
-    public static final int WIFI_ENABLE_TIMEOUT = 30;
-    public static final int WIFI_DISABLE_TIMEOUT = 20;
+    public static final int WIFI_ENABLE_TIMEOUT = 60;
+    public static final int WIFI_DISABLE_TIMEOUT = 30;
 
     public static final String INTENT_SERVICE_STATE = "wifeye.intent.service.state";
     public static final String EXTRAS_SERVICE_STATE = "wifeye.intent.param.service.state";
