1b25c833447d7b31bab30b3d3776a840d47d27a4
Mahan Rad <mahorad@yahoo.com>
2017-08-10 22:50:23 +0200

    start service from activity

--- app/src/main/java/mahorad/com/wifeye/ui/home/HomeActivity.java
+++ app/src/main/java/mahorad/com/wifeye/ui/home/HomeActivity.java
@@ -1,15 +1,25 @@
 package mahorad.com.wifeye.ui.home;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import mahorad.com.wifeye.R;
+import mahorad.com.wifeye.base.BaseActivity;
+import mahorad.com.wifeye.service.EngineService;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Intent intent = new Intent(this, EngineService.class);
+        startService(intent);
+    }
+
+    @Override
+    protected void injectDependencies() {
+        component().inject(this);
     }
 }
